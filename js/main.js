470bcfd6b8d73c44bbd03e28850af5030c37b615
DavidPreidikman <davidpreidikman95@gmail.com>
2023-04-18 13:19:15 -0300

    PreEntrega3-Preidikman

--- js/main.js
+++ js/main.js
@@ -9,42 +9,112 @@ function saludar(){
 
 saludar ()
 
+/* Selectores */
+const listaProductos = document.querySelector('#lista-productos');
+const tableCarrito = document.querySelector("#lista-carrito tbody");
+const btnVaciarCarrito = document.querySelector('#vaciar-carrito');
+let carrito = [];
 
-class Sneaker {
-	constructor (id, marca, modelo, sexo, precio) {
-		this.id = id;
-		this.marca = marca.toUpperCase();
-		this.modelo = modelo.toUpperCase();
-		this.sexo = sexo;
-		this.precio = precio;
-	}
+/* Listeners */
+listaProductos.addEventListener('click', agregarProducto);
+tableCarrito.addEventListener('click', borrarProducto);
+btnVaciarCarrito.addEventListener('click', vaciarCarrito);
+document.addEventListener('DOMContentLoaded', () => {
+
+    if (JSON.parse(localStorage.getItem('carrito'))) {
+        carrito = JSON.parse(localStorage.getItem('carrito'));
+        insertarCarritoHTML();
+    }
+});
+
+function vaciarCarrito() {
+    carrito = [];
+    insertarCarritoHTML();
 }
 
-const sneaker1 = new Sneaker(1, "Nike", "Air Jordan 1","Men's Shoes", 140);
-const sneaker2 = new Sneaker(2, "Nike", "Air Force 1 '07", "Men's Shoes", 110);
-const sneaker3 = new Sneaker(3, "Nike", "Air Max 90", "Men's Shoes", 100);
-const sneaker4 = new Sneaker(4, "Nike", "Air Blazer Mid '77", "Men's Shoes", 120);
-const sneaker5 = new Sneaker(5, "Nike", "P-6000", "Men's Shoes", 160);
-const sneaker6 = new Sneaker(6, "Nike", "Cortez", "Men's Shoes", 110);
-const sneaker7 = new Sneaker(7, "Nike", "Jordan Delta 3", "Women's Shoes", 130);
-const sneaker8 = new Sneaker(8, "Nike", "Air Jordan Low", "Women's Shoes", 120);
-const sneaker9 = new Sneaker(9, "Nike", "Air Jordan Elevate", "Women's Shoes", 130);
 
-const Carrito=[]
+function borrarProducto(e) {
+    e.preventDefault();
 
-function agregarACarrito(){
-	Carrito.push(sneaker1,sneaker8); 
+    if (e.target.classList.contains("borrar-producto")) {
+        
+        const productoId = e.target.getAttribute('data-id');
+        carrito = carrito.filter(producto => producto.id !== productoId);
+        insertarCarritoHTML();
+    }
 }
 
-agregarACarrito()
+function agregarProducto(e) {
+    e.preventDefault();
 
-function verCarrito(){
-	let total = 0;
-	for(elemento of Carrito){
-		console.log("ID: "+elemento.id +" ","Product: "+elemento.marca+" "+elemento.modelo+" "+elemento.sexo);
-		total += elemento.precio;
-	}
-	console.log("You're buying " + Carrito.length+ " product/s Your total is: $" + total);
+    if(e.target.classList.contains("agregar-carrito")){ 
+        const cardProducto = e.target.parentElement.parentElement;
+        
+        obtenerDatosProducto(cardProducto);
+    }
 }
 
-verCarrito()
+function obtenerDatosProducto(cardProducto) {
+
+    const productoAgregado = {
+        imagen: cardProducto.querySelector('img').src,
+        nombre: cardProducto.querySelector('h5').textContent,
+        precio: cardProducto.querySelector('.precio').textContent,
+        cantidad: 1,
+        id: cardProducto.querySelector('a').getAttribute('data-id')
+    }
+   
+    const existe = carrito.some(producto => producto.id === productoAgregado.id);
+
+    if (existe) {
+        const productos = carrito.map(producto => {
+            if (producto.id === productoAgregado.id) {
+                producto.cantidad++;
+                producto.precio = `$${Number(productoAgregado.precio.slice(1)) * producto.cantidad}`
+            } else {
+            }
+            return producto;
+        });
+        carrito = [...productos];
+    } else {
+        carrito = [...carrito, productoAgregado];
+    }
+
+   insertarCarritoHTML();
+}
+
+function insertarCarritoHTML() {
+
+    borrarCarritoHTML();
+
+    carrito.forEach(producto => {
+        /* Destructuring de objetos */
+        const { imagen, nombre, precio, cantidad, id } = producto;
+
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+            <img src="${imagen}" width='100%'>
+        </td>
+        <td>${nombre}</td>
+        <td>${precio}</td>
+        <td>${cantidad}</td>
+        <td>
+            <a href="#" class="borrar-producto" data-id="${id}">X</a>
+        </td>
+        `
+        tableCarrito.appendChild(row);
+    });
+    guardarCarritoStorage();
+}
+
+function borrarCarritoHTML() {
+
+    while (tableCarrito.firstChild) {
+        tableCarrito.removeChild(tableCarrito.firstChild);
+    }
+}
+
+function guardarCarritoStorage() {
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
